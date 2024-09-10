7cfba456af60f34da67c44d848ba3ca9b321ec16
Intybyte <vaan.testwork@gmail.com>
2024-09-10 11:58:57 +0200

    Make timer configurable

--- src/main/java/me/vaan/cannonsigns/CannonSigns.java
+++ src/main/java/me/vaan/cannonsigns/CannonSigns.java
@@ -3,6 +3,7 @@ package me.vaan.cannonsigns;
 import at.pavlov.cannons.cannon.CannonManager;
 import me.vaan.cannonsigns.listeners.SignListener;
 import me.vaan.cannonsigns.utils.SignUpdateUtils;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CannonSigns extends JavaPlugin {
@@ -12,6 +13,8 @@ public final class CannonSigns extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+        saveDefaultConfig();
+        FileConfiguration config = getConfig();
         this.getServer().getPluginManager().registerEvents(new SignListener(), this);
 
         getServer().getScheduler().runTaskTimer(this, () -> {
@@ -19,7 +22,7 @@ public final class CannonSigns extends JavaPlugin {
             for (var cannon : cannonList.values()) {
                 SignUpdateUtils.updateCannonSigns(cannon);
             }
-        }, 0, 10);
+        }, 0, config.getInt("check-every", 10));
     }
 
     public static CannonSigns getInstance() {
